9808eb1509809b2a28eedfb688eeab178b92cf74
Daniel Rotstein Gonçalves <97666187+rotsteindaniel@users.noreply.github.com>
2022-07-14 16:10:11 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import { Header } from "./components/Layouts/Header";
+import { Meals } from "./components/Meals/Meals";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </>
+  );
+}
+
+export default App;
